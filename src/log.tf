0e274634bda8bb491594de08994fe679c87f8875
mclacore <michael@massdriver.cloud>
2022-09-30 13:11:57 -0600

    Azure FHIR service bundle build

---
+++ src/log.tf
@@ -0,0 +1,48 @@
+resource "azurerm_storage_account" "diag-log" {
+  count                     = var.logging.enable_logging ? 1 : 0
+  name                      = "${local.alphanumeric_name}diaglog"
+  resource_group_name       = azurerm_resource_group.main.name
+  location                  = azurerm_resource_group.main.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
+  enable_https_traffic_only = true
+  min_tls_version           = "TLS1_2"
+  tags                      = var.md_metadata.default_tags
+
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_rules {
+    default_action = "Deny"
+    bypass         = ["AzureServices", "Logging"]
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diag-log" {
+  count              = var.logging.enable_logging ? 1 : 0
+  name               = var.md_metadata.name_prefix
+  target_resource_id = azurerm_healthcare_fhir_service.main.id
+  storage_account_id = azurerm_storage_account.diag-log[0].id
+
+  log {
+    category = "AuditLogs"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = 7
+    }
+  }
+}
---
+++ src/main.tf
@@ -0,0 +1,44 @@
+locals {
+  max_length        = 24
+  alphanumeric_name = substr(replace(var.md_metadata.name_prefix, "/[^a-z0-9]/", ""), 0, local.max_length)
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.md_metadata.name_prefix
+  location = var.database.region
+  tags     = var.md_metadata.default_tags
+}
+
+resource "azurerm_healthcare_workspace" "main" {
+  name                = local.alphanumeric_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = var.md_metadata.default_tags
+}
+
+resource "azurerm_healthcare_fhir_service" "main" {
+  name                                      = var.md_metadata.name_prefix
+  location                                  = azurerm_resource_group.main.location
+  resource_group_name                       = azurerm_resource_group.main.name
+  workspace_id                              = azurerm_healthcare_workspace.main.id
+  kind                                      = "fhir-R4"
+  configuration_export_storage_account_name = var.database.export_data ? "${local.alphanumeric_name}export" : 0
+  tags                                      = var.md_metadata.default_tags
+
+  authentication {
+    authority = "https://login.microsoftonline.com/${var.azure_service_principal.data.tenant_id}"
+    audience  = "https://${var.md_metadata.name_prefix}.fhir.azurehealthcareapis.com"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  dynamic "oci_artifact" {
+    for_each = { for r in var.registry : r.login_server => r }
+    content {
+      login_server = oci_artifact.value.login_server
+      image_name   = oci_artifact.value.image_name
+    }
+  }
+}
